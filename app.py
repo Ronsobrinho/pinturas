39b9e11373808354de463124ca49a76a5ed04388
ronsobrinho <jcsvieira30@gmail.com>
2025-04-19 22:11:56 -0300

    fix: ajustes para deploy no Railway

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def static_proxy(path):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='0.0.0.0', port=port, debug=False) 
